8ce7ad5117a0a2479c882ce11bdf03ce9a60c00b
responsaeuris <responsaeuris@gmail.com>
2021-12-02 14:53:44 +0000

    7fbdfa4af0dfdc3c482c9261e98cefe2d7de0e68 Merge branch _release_0.2.6_ CI fingerprint

--- app/routes/status/statusRoute.js
+++ app/routes/status/statusRoute.js
@@ -1,6 +1,6 @@
 const status = async () => {
-  const ciCommit = 'CI_PUTS_HERE_LAST_GIT_COMMIT'
-  const lastDeploy = 'CI_PUTS_HERE_DEPLOY_DATE'
+  const ciCommit = '7fbdfa4af0dfdc3c482c9261e98cefe2d7de0e68 Merge branch _release_0.2.6_'
+  const lastDeploy = 'Thu Dec  2 14:53:07 UTC 2021'
 
   return `ok! Plugin Core released on ${lastDeploy}, last commit was "${ciCommit}"`
 }
